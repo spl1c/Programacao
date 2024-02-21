c3802586f544cb054be77936ffc592594f731a7d
spl1ce <spl1ce5225@gmail.com>
2024-02-21 16:31:54 +0000

    fire skr skr

---
+++ lab_2/7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) 
+{   
+    double number = 3.9438426527542; 
+    int n;
+    double rounded_number;
+    // input de um numero real
+    printf("Escreva um numero real a sua escolha: ");
+    scanf("%f", &number);
+    // input de n, numero de casas decimais
+    printf("Escreva um numero n que seja entre 1 e 7: ");
+    scanf("%d", &n);
+
+    // aqui fazia alguma cena pa aproximar
+
+
+    printf("O valor esperado é %.*f", n, number);
+    printf("O valor obtido é %f", rounded_number);
+
+}
