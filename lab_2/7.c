cd358bbaf6316909448073163a706df94cf32f0c
valentimcasimiro <160672735+valentimcasimiro@users.noreply.github.com>
2024-02-21 20:10:56 +0000

    Add files via upload
    7

--- lab_2/7.c
+++ lab_2/7.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 
+float potencia(int p){
+    float i;
+    for (i = 0; i <= p; i++)
+    {
+        i *= 10.0;
+    }
+    
+    return i;
+}
 int main(void) 
 {   
-    double number = 3.9438426527542; 
+    float number, rounded_number, pot;
     int n;
-    double rounded_number;
+    
     // input de um numero real
     printf("Escreva um numero real a sua escolha: ");
     scanf("%f", &number);
     // input de n, numero de casas decimais
-    printf("Escreva um numero n que seja entre 1 e 7: ");
+    printf("Escreva um numero que seja entre 1 e 7: ");
     scanf("%d", &n);
-
+    while (n<1 || n> 7)
+    {
+        printf("Volta a escrever o numero num intervalo entre 1 e 7: ");
+        scanf("%d", &n);
+    }
+    
     // aqui fazia alguma cena pa aproximar
-
+    pot = potencia(n);
+    rounded_number = (number*pot + 0,5)/pot;
 
     printf("O valor esperado é %.*f", n, number);
     printf("O valor obtido é %f", rounded_number);
-
+    return 0;
 }
