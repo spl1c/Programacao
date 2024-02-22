1ab9aa5ad86d2cb19514bd65c3a8f0fa2140161e
spl1ce <spl1ce5225@gmail.com>
2024-02-22 15:22:19 +0000

    Fixed 2.c

--- lab_2/aula/2.c
+++ lab_2/aula/2.c
@@ -14,14 +14,13 @@ int main()
     printf("Please enter the inicial velocity(m/s) and the ceiling height (m) with a space in between: ");
     scanf("%f %f", &v, &max_h);
 
-    while (max_h>h) // while loop that runs until the height
+    while (max_h>h && ang <=90) // while loop that runs until the height
     {
         rad = ang * ( 3.14 / 180.0 );
-        h = pow(v, 2)*pow(sin(rad), 2)/2*g;
+        h = pow(v, 2)*pow(sin(rad), 2)/(2*g);
         ang++;
     }
-
+    ang -= 1;
     printf("0.000 %.3f", ang);
-
 
 }
