ac6d805b18e30e0bb1bc5d0bb327a7fb6e36e9c1
spl1ce <spl1ce5225@gmail.com>
2024-02-22 01:50:07 +0000

    Fixed 7
    O 7 não estava a funcionar, corrigi e adicionei comentarios

--- lab_2/7.c
+++ lab_2/7.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 float potencia(int p){
-    float i;
-    for (i = 0; i <= p; i++)
+    float f = 1;
+    for (int i = 0; i < p; i++)
     {
-        i *= 10.0;
+        f *= 10.0;
     }
     
-    return i;
+    return f;
 }
 int main(void) 
 {   
@@ -23,14 +23,31 @@ int main(void)
   
     while (n<1 || n> 7)
     {
-        printf("Volta a escrever o numero num intervalo entre 1 e 7: ");
+        printf("Volte a escrever o numero num intervalo entre 1 e 7: ");
         scanf("%d", &n);
     }
     
-    // calculo da aproximação do decimal
+    // calculo da aproximação do decimal (explicação)
+
+    // criamos uma potencia de base 10 com o tamanho do numero n, caso n seja 2, então pot será 100
     pot = potencia(n);
-    rounded_number = (number*pot + 0,5)/pot;
-    //printf do resultado pedido e do sem a especificaçao da conta
+    // multiplicamos o numero por 'pot' de modo a mover o ponto decimal para a casa que queremos aproximar
+    rounded_number *= pot;
+    // somamos 0.5 caso a casa decimal seja 0.5 ou mais seja aproximada para o inteiro acima quando a convertermos para inteiro
+    rounded_number += 0.5;
+    // finalmente convertemos em inteiro que vai aproximar o numero em direcao a 0
+    rounded_number = (int)rounded_number;
+    // dividimos pela potencia para colocar o ponto decimal onde estava no inicio
+    rounded_number /= pot;
+
+    // ex: 
+    // numero: 4.36672, queremos reduzi-lo a 2 casas decimais
+    // pot seria 100, 4.36672 * pot = 436.672
+    // 436.672 + 0.5 = 437.172, int(437.172) é 437
+    // finalmente 437 / pot = 4.37 ficando corretamente proximado
+
+    rounded_number = (int)(number*pot + 0.5)/pot;
+    // printf do resultado esperado e do resultado do algoritmo desenvolvido
     printf("O valor esperado é %.*f", n, number);
     printf("O valor obtido é %f", rounded_number);
     return 0;
