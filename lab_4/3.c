cc4f3a2a5cbdd8332bcd30e69e6a5622743feb0a
spl1ce <spl1ce5225@gmail.com>
2024-03-07 10:33:12 +0000

    exercicio 3 feito

--- lab_4/3.c
+++ lab_4/3.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 
-int histogram(float vect[], int k)
+
+
+int *histogram(float vect[], int k, int max, int min)
 {
-    int results[k];
-    float min = vect[0];
-    float max = vect[0];
+    int* results = (int *)malloc(sizeof(int)*k);
+/*    float min = vect[1];
+    float max = vect[1];
 
-
-    for(int i = 0; i < ((sizeof vect)/(sizeof *vect)); i++)
+      for(int i = 1; i < vect[0]; i++)
     {
         if (vect[i] < min)
         {
@@ -19,14 +20,14 @@ int histogram(float vect[], int k)
         {
             max = vect[i];
         }
-    }
+    }*/
 
-    float largura = (max-min)/k;
+    float largura = (float)(max-min)/k;
     int indice;
 
-    for(int j=0; j < k; j++)
+    for(int j=1; j <= vect[0]; j++)
     {
-        indice = (int)vect[j]/largura;
+        indice = ((vect[j]-min)/largura);
         results[indice]++;
     }
 
@@ -35,33 +36,39 @@ int histogram(float vect[], int k)
 
 void printhistogram(int results[], int k)
 {
+    int j;
     for(int i = 0; i<k;i++)
     {
         printf("%d ", i);
-        for(int j=0; j<results[i];++j){
+        for(j=0; j<results[i];++j){
             printf("*");
         }
+        printf("\n");
     }
 }
 int main()
 {   
-    int k;
-    int n;
+    int k, q, min, max;
     int div = 1;
     printf("Introduza o número de intervalos: ");
     scanf("%d", &k);
     printf("\nIntroduza a quantidade de valores: ");
-    scanf("%d", &n);
+    scanf("%d", &q);
+    printf("Introduza uma valor mínimo: ");
+    scanf("%d", &min);
+    printf("Introduza um valor máximo: ");
+    scanf("%d", &max);
 
-    float vect[n];
+    float vect[q+1];
 
-    for(int i=0; i<n; i++)
+    for(int i=0; i<q; i++)
     {
-        vect[i] = (float) rand();
+        vect[i+1] = (float) (rand()%100)/100;
         
     }
+    vect[0] = (float)q;
 
-    int results = histogram(vect, k);
+    int *results = histogram(vect, k, min, max);
     printhistogram(results, k);
 
 }
