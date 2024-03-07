99d7a39dcc6ba39f06188453521b79a06497408c
valentimcasimiro <valentim.casimiro@tecnico.ulisboa.pt>
2024-03-07 08:32:01 +0000

    aula

--- lab4/1.c
+++ lab4/1.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 int rand1, rand2;
-
+//Desenvolver os numeors random
 void random(){
     rand1 = rand()%9 + 1;
     rand2 = rand()%9 + 1;
 }
-
+//Main com multiplicação das contas
 int main()
 {
-    int res, aluno;
+    int res, aluno, i = 1;
+    float percentagem;
     srand(time(NULL));
     random(rand1, rand2);
     res = rand1 * rand2;
+    //Escrever a resposta dada pelo aluno
     printf("Escreve o resultado de %d*%d= ", rand1, rand2);
     scanf("%d", &aluno);
+    //Caso errado, volta a tentar
     while(aluno!=res){
         printf("Erraste, tenta outra vez %d*%d= ", rand1, rand2);
         scanf("%d", &aluno);
+        i++;
     }
-    printf("Acertaste o resultado e %d", res);
+    percentagem = 1/(float)i * 100;
+
+    printf("Acertaste o resultado e %d, tens uma precisão de %f %%\n", res, percentagem);
     return 0;
 }
